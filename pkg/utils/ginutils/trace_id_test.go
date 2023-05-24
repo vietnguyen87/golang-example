76884fc050e0fe764e039b0c5b34bcd31193c65d
vietnguyen87 <vietnt@marathon.edu.vn>
2023-05-24 10:58:49 +0700

    Add Linter go.

--- pkg/utils/ginutils/trace_id_test.go
+++ pkg/utils/ginutils/trace_id_test.go
@@ -1,15 +1,8 @@
 package ginutils
 
-import (
-	"fmt"
-	"testing"
-
-	"github.com/rs/xid"
-)
-
-func TestTraceID(t *testing.T) {
-	id := xid.New()
-	id.Time()
-	traceID := fmt.Sprintf("%s_%s", id.Time().Format(formatDate), id.String())
-	t.Log(traceID)
-}
+//func TestTraceID(t *testing.T) {
+//	id := xid.New()
+//	id.Time()
+//	traceID := fmt.Sprintf("%s_%s", id.Time().Format(formatDate), id.String())
+//	t.Log(traceID)
+//}
